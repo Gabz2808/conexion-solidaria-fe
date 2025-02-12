c49008e81eff004748e3301dae84054b7e637462
Gabriel Mairena <gabrielgranera28@hotmail.com>
2025-02-11 21:04:26 -0600

    Nuevos cambios

--- Frontend/conexion-solidaria-fe/src/Components/Navbar.tsx
+++ Frontend/conexion-solidaria-fe/src/Components/Navbar.tsx
@@ -1,28 +1,51 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { SearchBar } from "./UI/SearchBar";
 import BellIcon from "./../assets/icons/bell.svg";
+import UserIcon from "./../assets/icons/user.svg";
 import LoginIcon from "./../assets/icons/sign-in-alt.svg";
+import LogoutIcon from "./../assets/icons/exit.svg";
 
 import "../assets/styles/Navbar.css";
 
 const Navbar: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Logo</Link>
-        </li>
-        <li>
-          <SearchBar />
-        </li>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-        <li>
-          <img src={BellIcon} alt="Notificaciones" className="navbar-icon" />
-        </li>
-        <li>
-          <img src={LoginIcon} alt="Login" className="navbar-icon" />
-        </li>
-      </ul>
+  const toggleLogin = () => {
+    setIsLoggedIn((prev) => !prev);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/">Logo</Link>
+      </div>
+      <div className="navbar-center">
+        <SearchBar />
+      </div>
+      <div className="navbar-right">
+        {isLoggedIn ? (
+          <>
+            <img src={BellIcon} alt="Notificaciones" className="navbar-icon" />
+            <img
+              src={UserIcon}
+              alt="Perfil de usuario"
+              className="navbar-icon"
+            />
+            <button onClick={toggleLogin}>
+              <img
+                src={LogoutIcon}
+                alt="Cerrar sesión"
+                className="navbar-icon"
+              />
+            </button>
+          </>
+        ) : (
+          <button onClick={toggleLogin}>
+            <img src={LoginIcon} alt="Iniciar sesión" className="navbar-icon" />
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
--- Frontend/conexion-solidaria-fe/src/Components/UI/SearchBar.tsx
+++ Frontend/conexion-solidaria-fe/src/Components/UI/SearchBar.tsx
@@ -1,15 +1,35 @@
+import SearchIcon from "../../assets/icons/search.svg";
+
 const SearchBar = () => (
-    <form action="/" method="get">
-        <label htmlFor="header-search">
-        </label>
-        <input
-            type="text"
-            id="header-search"
-            
-            name="s" 
-        />
-        <button type="submit">Search</button>
-    </form>
+  <form action="/" method="get">
+    <label htmlFor="header-search" hidden>
+      Buscar
+    </label>
+    <input
+      type="text"
+      id="header-search"
+      name="s"
+      style={{
+        width: "200px",
+        backgroundColor: "#f2f2f2",
+        color: "black",
+        borderRadius: "20px",
+        padding: "5px",
+        border: "none",
+        outline: "none",
+      }}
+    />
+    <button type="submit">
+      <img
+        src={SearchIcon}
+        alt="search"
+        className="navbar-icon"
+        style={{
+          filter: "none",
+        }}
+      />
+    </button>
+  </form>
 );
- 
+
 export { SearchBar };
